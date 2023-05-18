3329ef7c3abed1afefa68636a79b8c81b2dd9f93
vishnusingh0101 <singh.vishnu8369@gmail.com>
2023-05-18 17:15:51 +0530

    integration of frontend with backend

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const userRoute = require('./routes/user');
 const sequelize = require('./util/database');
 
 const app = express();
 
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
 app.use(bodyParser.json({extended: false}));
 
 app.use(userRoute);
--- controllers/usercontrols.js
+++ controllers/usercontrols.js
@@ -6,11 +6,11 @@ const signup = async (req, res, next) => {
         const { name, mail, phone, password } = req.body;
         const existingmail = await User.findOne({where: {mail}});
         if(existingmail) {
-            return res.status(409).json({message: "Email already exist!"});
+            return res.status(201).json({message: "Email already exist!"});
         }
         const existingphone = await User.findOne({where: {phone}});
         if(existingphone) {
-            return res.status(409).json({message: "Phone Number already exist!"});
+            return res.status(201).json({message: "Phone Number already exist!"});
         }
         bcrypt.hash(password, 10, async (err, hash) => {
             await User.create({name, mail, phone, password:hash});
